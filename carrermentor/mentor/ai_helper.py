1f6ddc20ebb4344a8ba626d7dde11ca9701c1290
krishnakowshikmeesala <krishnakowshikmeesala@gmail.com>
2026-07-18 08:28:11 +0530

    Update ai_helper.py

--- carrermentor/mentor/ai_helper.py
+++ carrermentor/mentor/ai_helper.py
@@ -3,21 +3,26 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 def generate_career_report(student):
     prompt = f"""
     You are an expert AI Career Mentor.
-    Student Details:
+
     Name: {student.full_name}
     Age: {student.age}
     Education: {student.education}
     Location: {student.location}
-    Interests:{student.interests}
-    Strengths:{student.strengths}
-    Career Goal:{student.career_goal}
-    Learning Preference:{student.learning_preference}
-    Challenge:{student.challenge}
-    Dream Career:{student.dream_career}
+    Interests: {student.interests}
+    Strengths: {student.strengths}
+    Career Goal: {student.career_goal}
+    Learning Preference: {student.learning_preference}
+    Challenge: {student.challenge}
+    Dream Career: {student.dream_career}
+
     Generate:
     1. Recommended Career
     2. Why This Career
@@ -26,8 +31,7 @@ def generate_career_report(student):
     5. Six Month Roadmap
     6. Free Learning Resources
     """
-    response = client.models.generate_content(
-        model="gemini-3.5-flash",
-        contents=prompt
-    )
+
+    response = model.generate_content(prompt)
+
     return response.text
